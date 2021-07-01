3212399b105ccbf6b24206b1c0d95d69d15eb57b
IHprogram <ihm20202020@gmail.com>
2021-07-01 14:42:35 +0900

    CSSの設定を追加

--- src/App.js
+++ src/App.js
@@ -3,29 +3,32 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import memories from './images/top.png';
 import Posts from './components/Posts/Posts.js';
 import Form from './components/Form/Form.js';
+import useStyles from './styles';
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <Container maxidth="lg">
-      <AppBar position="static">
-        <Typography variant="h2" align="center">
-          Memories
-        </Typography>
-        <img src={memories} alt="memories" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
+    <div>
+      <Container maxidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography variant="h2" align="center">
+            Memories
+          </Typography>
+          <img src={memories} alt="memories" height="60" />
+        </AppBar>
+        <Grow in>
           <Grid container justify="space-between" alignItems="stretch" spacing={3} >
-            <Grid item xs={12} sm={7} >
+            <Grid item xs={12} sm={7}>
               <Posts />
             </Grid>
             <Grid item xs={12} sm={7} >
               <Form />
             </Grid>
           </Grid>
-        </Container>
-      </Grow>
-    </Container >
+        </Grow>
+      </Container>
+    </div>
   )
 }
 
---
+++ src/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    display: 'flex',
+    justifyContent: 'center',
+    aliginItems: 'center',
+    flexDirection: 'row',
+    borderRadius: 15
+  }
+}))
