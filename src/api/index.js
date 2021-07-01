6c4da2fff17d05c0660995d00d2d70805fccb72f
IHprogram <ihm20202020@gmail.com>
2021-07-01 17:27:03 +0900

    データベースとやりとり(データ取得や保存なそ)をするためのファイルを作成

--- src/api/index.js
+++ src/api/index.js
@@ -4,3 +4,5 @@ const url = 'http://localhost:5000/posts';
 
 // axiosを使って、投稿内容を取得
 export const fetchPosts = () => axios.get(url);
+// データベースへの保存を行う
+export const createPost = (newPost) => axios.post(url, newPost);
