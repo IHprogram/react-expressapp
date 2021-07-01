08ab8185473eb80e450222092dda3ebb8c3a2065
IHprogram <ihm20202020@gmail.com>
2021-07-01 17:28:32 +0900

    storeのstateを呼び出すためuseSelectorを追加

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import Post from './Post/Post';
 
 function Posts() {
+  const postsState = useSelector((state) => state.posts);
+
+  console.log(postsState);
   return (
     <div>
       <h1>投稿一覧</h1>
+      <Post />
     </div>
   )
 }
